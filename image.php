296fee0eb3086e7b01532ef972287a7d52214d4d
svbrburgman <49238647+svbrburgman@users.noreply.github.com>
2019-04-03 15:04:59 +0200

    Update image.php

--- image.php
+++ image.php
@@ -6,11 +6,11 @@ $white = imagecolorallocate($im, 255, 255, 255);
 $deployment = getenv("COLOR");
 
 if ( $deployment == 'blue') {
-  $color = imagecolorallocate($im, 0, 0, 244);
+  $color = imagecolorallocate($im, 0, 0, 100);
 } elseif ($deployment == 'green')  {
-  $color = imagecolorallocate($im, 0, 244, 0);
+  $color = imagecolorallocate($im, 0, 100, 0);
 } else {
-  $color = imagecolorallocate($im, 0, 0, 244);
+  $color = imagecolorallocate($im, 0, 0, 100);
 }
 
 // Draw a filled rectangle
